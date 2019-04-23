116b8e3be2bae73d42bb593a5326ff8741328266
Samuel Rönnqvist <samuel@ronnqvist.fi>
2019-04-23 10:23:57 +0300

    timediff bugfix

--- create_training_data.py
+++ create_training_data.py
@@ -3,10 +3,15 @@ import sys
 import collections
 import re
 
+
 def timediff(prior, latter):
     mins, secs = str(prior).strip().split('.')
+    if len(secs) == 1:
+        secs += '0'
     prior_secs = int(mins)*60+int(secs)
     mins, secs = str(latter).strip().split('.')
+    if len(secs) == 1:
+        secs += '0'
     latter_secs = int(mins)*60+int(secs)
     diff = latter_secs - prior_secs
     diff_mins = diff//60
@@ -14,7 +19,7 @@ def timediff(prior, latter):
     return "%d.%d" % (diff_mins, diff_secs)
 
 
-def generate_input(event, context):
+def generate_input(event, context, xml_style=True):
     out = []
     # Define selection and order of information for training input
     if event['Type'] == 'Lopputulos':
@@ -23,7 +28,7 @@ def generate_input(event, context):
         out.append(('guest', event['Guest']))
         out.append(('score', event['Score']))
         out.append(('periods', event['Periods']))
-        out.append(('time', event['Time']))
+        #out.append(('time', event['Time']))
         context['final_score'] = event['Score']
     elif event['Type'] == 'Maali':
         out.append(('type', 'goal'))
@@ -32,8 +37,9 @@ def generate_input(event, context):
         out.append(('player', event['Player']))
         out.append(('assist', event['Assist']))
         out.append(('time', event['Time']))
-        if 'last_goal_time' in context:
-            out.append(('timediff', timediff(context['last_goal_time'], event['Time'])))
+        if 'time_diff' in event:
+            #out.append(('timediff', timediff(context['last_goal_time'], event['Time'])))
+            out.append(('timediff', event['time_diff']))
         out.append(('period', int(float(event['Time'])//20+1)))
         goal_types = []
         if 'final_score' in context and event['Score'] == context['final_score']:
@@ -43,7 +49,7 @@ def generate_input(event, context):
         if goal_types:
             out.append(('goaltype', ', '.join(goal_types)))
         out.append(('abbrevs', event['Abbreviations']))
-        context['last_goal_time'] = event['Time']
+        #context['last_goal_time'] = event['Time']
     elif event['Type'] == 'J\u00e4\u00e4hy':
         out.append(('type', 'penalty'))
         out.append(('team', event['Team']))
@@ -64,7 +70,10 @@ def generate_input(event, context):
             if key not in dict(out):
                 out.append((key, event[key]))
 
-    return ' '.join(['<%s>%s</%s>' % (k,v,k) for k,v in out])
+    if xml_style:
+        return ' '.join(['<%s>%s</%s>' % (k,v,k) for k,v in out])
+    else:
+        return ', '.join(['%s=\'%s\'' % (k,v) for k,v in out])
 
 
 if len(sys.argv) < 2:
@@ -73,23 +82,35 @@ if len(sys.argv) < 2:
 
 meta = json.load(open(sys.argv[1]))
 
-event_pat = re.compile("^E\d+$")
+event_ref_pat = re.compile("^E\d+$")
 
 for key in meta:
+    # Calculate time diff between goals
+    last_goal_time = None
+    for event in meta[key]['events']:
+        if event['Type'] == 'Maali':
+            if last_goal_time:
+                event['time_diff'] = timediff(last_goal_time, event['Time'])
+            last_goal_time = event['Time']
+
+    # Collect events with mentions
     entries = collections.defaultdict(lambda: [])
     for event in meta[key]['events']:
         if 'text' in event:
             #print(event)
-            if event_pat.search(event['text']):
+            if event_ref_pat.search(event['text']): # Is event reference?
                 entries[event['text']].append(event)
             else:
                 entries[event['event_idx']].append(event)
 
-    if entries:
-        print()
-        print("GAME:", key)
-        context = {}
+    if not entries:
+        continue
 
+    print()
+    print("GAME:", key)
+    context = {}
+
+    # Identify deciding goal
     last_score = None
     loosing_score = None
     for event in meta[key]['events'][::-1]:
@@ -104,11 +125,12 @@ for key in meta:
                 break
             last_score = event['Score']
 
+    # Print results
     for idx, events in entries.items():
         text = None
         for event in events:
-            print('   IN:', generate_input(event, context))
-            if not event_pat.search(event['text']):
+            print('   IN:', generate_input(event, context, xml_style=False))
+            if not event_ref_pat.search(event['text']):
                 text = event['text']
 
         print('   OUT:', text)
