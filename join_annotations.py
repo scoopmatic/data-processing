231330b3042459f1a96ce050a4a919ce70c890d2
Samuel Rönnqvist <samuel.ronnqvist@gmail.com>
2019-06-18 09:37:54 +0300

    option include unaligned events, for data augmentation

--- join_annotations.py
+++ join_annotations.py
@@ -10,8 +10,11 @@ if len(sys.argv) < 4:
 meta = json.load(open(sys.argv[1]))
 anno_f = open(sys.argv[2])
 
+INCLUDE_UNALIGNED = False #True
 cnt_game_begs, cnt_game_ends, cnt_events = 0, 0, 0
 cnt_games_with_text, cnt_events_with_text, cnt_events_without_text = 0, 0, 0
+cnt_events_in_nonempty_game = 0
+cnt_events_in_game = 0
 cnt_event_types = collections.defaultdict(lambda: 0)
 PATIENCE = 100
 patience_left = PATIENCE
@@ -25,6 +28,7 @@ for i, line in enumerate(anno_f):
         #assert event_texts == {}
         event_texts = {}
         cnt_game_begs += 1
+        cnt_events_in_game = 0
 
     elif line.startswith("# IDX ="):
         key = line.split('=')[1].strip()
@@ -52,12 +56,16 @@ for i, line in enumerate(anno_f):
                 #event_texts = {}
         if game_has_text:
             cnt_games_with_text += 1
+            cnt_events_in_nonempty_game += cnt_events_in_game
+
+        cnt_events_in_game = 0
         if patience_left <= 0:
             print("Empty event annotations: patience elapsed at line", i, file=sys.stderr)
             break
     else:
         match = event_pat.search(line)
         if match:
+            cnt_events_in_game += 1
             cnt_events += 1
             if event_pat_empty.search(line):
                 cnt_events_without_text += 1
@@ -68,7 +76,7 @@ for i, line in enumerate(anno_f):
             except IndexError:
                 print("Parse error on line %d in %s: %s" % (i, sys.argv[2], line), file=sys.stderr)
                 raise
-            if event_text:
+            if event_text or INCLUDE_UNALIGNED:
                 cnt_events_with_text += 1
                 event_texts[event_id] = event_text
                 patience_left = PATIENCE
@@ -77,8 +85,10 @@ for i, line in enumerate(anno_f):
 
 
 
-json.dump(meta, open(sys.argv[3], 'w'), indent=2, ensure_ascii=False, sort_keys=False)
+json.dump(meta, open(sys.argv[3], 'w'), indent=2, sort_keys=False)
 print("Found %d (%d) games, %d events" % (cnt_game_begs, cnt_game_ends, cnt_events))
 print("Text in %d games, %d events. Not in %d events (%d+%d=%d~%d)" % (cnt_games_with_text, cnt_events_with_text, cnt_events_without_text, cnt_events_without_text, cnt_events_with_text, cnt_events_without_text+cnt_events_with_text, cnt_events))
 for evtype in cnt_event_types:
     print("  ", cnt_event_types[evtype], evtype)
+
+print("cnt_events_in_nonempty_game",cnt_events_in_nonempty_game)
